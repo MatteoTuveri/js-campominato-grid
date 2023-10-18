97da4f37f7a6c4fe4466f8ac86367823a5b1bdc6
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-10-18 16:03:36 +0200

    add function for squares, script for generator and classes

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+/* *Consegna*
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di 
+azzurro ed emetto un messaggio in console con il numero della cella 
+cliccata.
+
+ */
+
+const start = document.querySelector('.btn');
+const playground = document.getElementById('playground');
+
+start.addEventListener('click',()=>{
+    
+    playground.innerHTML='';
+    start.innerHTML=`Retry`
+    for(let i=1;i<=100;i++)//squares generator
+    {
+        responsiveSquare(playground,'active',i);
+    }
+
+})
